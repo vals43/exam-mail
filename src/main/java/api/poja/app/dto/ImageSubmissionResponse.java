9de5b230488d33d2f0916aeb054338e82ee7c711
Vals43 <hei.teddy.3@gmail.com>
2026-07-13 09:09:09 +0300

    feat: add ImageSubmissionResponse DTO

---
+++ src/main/java/api/poja/app/dto/ImageSubmissionResponse.java
@@ -0,0 +1,7 @@
+package api.poja.app.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record ImageSubmissionResponse(UUID id, String nomFichier, String email, Instant createdAt) {
+}
