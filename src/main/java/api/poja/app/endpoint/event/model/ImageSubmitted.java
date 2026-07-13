47035e42a56b6271c31f5c1b8a576560200f6652
Vals43 <hei.teddy.3@gmail.com>
2026-07-13 09:12:27 +0300

    feat: add ImageSubmitted event

---
+++ src/main/java/api/poja/app/endpoint/event/model/ImageSubmitted.java
@@ -0,0 +1,33 @@
+package api.poja.app.endpoint.event.model;
+
+import java.time.Duration;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ImageSubmitted extends PojaEvent {
+
+  private UUID submissionId;
+  private String email;
+  private String s3Key;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
