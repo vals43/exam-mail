158272049179d01dbff40ef7026f429ed2934bb5
Vals43 <hei.teddy.3@gmail.com>
2026-07-13 08:56:25 +0300

    feat: add ImageSubmissionRepository

---
+++ src/main/java/api/poja/app/repository/ImageSubmissionRepository.java
@@ -0,0 +1,10 @@
+package api.poja.app.repository;
+
+import api.poja.app.entity.ImageSubmission;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageSubmissionRepository extends JpaRepository<ImageSubmission, UUID> {
+}
