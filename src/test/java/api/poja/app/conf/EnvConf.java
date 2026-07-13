75170feaaec2a1cfedf94a880c30be5bcf6a4692
Vals43 <hei.teddy.3@gmail.com>
2026-07-13 09:00:39 +0300

    fix: restore JPA deps removed by Poja, add H2 for tests

---
+++ src/test/java/api/poja/app/conf/EnvConf.java
@@ -0,0 +1,14 @@
+package api.poja.app.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class EnvConf {
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", () -> "jdbc:h2:mem:testdb");
+    registry.add("spring.datasource.driver-class-name", () -> "org.h2.Driver");
+    registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.H2Dialect");
+    registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    registry.add("spring.flyway.enabled", () -> "false");
+  }
+}
